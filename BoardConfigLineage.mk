810b09549f67b098b1ab4eadf6a4914b7c161aaf
Matt Filetto <matt.filetto@gmail.com>
2024-03-18 12:04:37 -0700

    gs101: Add build broken duplication flag
    -eliminates unnecessary duplicates in vendor/gms
    
    Change-Id: I6c40770fe8df8a8bd2d76bddae3373c0118a15f8
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -3,6 +3,7 @@
 # SPDX-FileCopyrightText: 2021-2024 The Calyx Institute
 # SPDX-License-Identifier: Apache-2.0
 #
+BUILD_BROKEN_DUP_RULES := true
 
 # Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
