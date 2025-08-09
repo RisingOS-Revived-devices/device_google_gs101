6c101dbd3f0a1c3dc53722d6004d14be45b6dbc7
LuK1337 <priv.luk@gmail.com>
2025-08-09 11:09:53 +0200

    gs101: Remove vendor/lineage device framework matrix inclusion
    See: I78da6340f38b93fdc4c3694cb8f431f387d16c0d
    Change-Id: I0b9f527de50ca02082be9b92375559f4893b2688

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -6,7 +6,6 @@
 BUILD_BROKEN_DUP_RULES := true
 
 # Manifests
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += device/google/gs101/manifest_radio.xml
 
 # Partitions
